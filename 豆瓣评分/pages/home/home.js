c7c0342b044e041b09b531417db393d8ca9e5eac
lucileu <363146203@qq.com>
2019-12-19 20:47:32 +0800

    merge

--- 豆瓣评分/pages/home/home.js
+++ 豆瓣评分/pages/home/home.js
@@ -7,7 +7,22 @@ Page({
     wx.getLocation({
       type: 'wgs84',
       success: (res) => {
-        console.log(res);
+        console.log(res.latitude, res.longitude);
+        wx.request({
+          url: 'https://api.map.baidu.com/reverse_geocoding/v3',
+          
+          data: {
+          ak: '77fhV1udNmmGLVwtRmh163g6I5PdURE8',
+          output: 'json',
+          coordtype: 'wgs84ll',
+          location: res.latitude + ',' + res.longitude
+          },
+          success: (res) => {
+            console.log(res)
+          },
+          fail: () => {}
+        });
+          
       },
       fail: () => {
         console.log('获取位置失败');
