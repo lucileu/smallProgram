041d4e17a327aeffad7c720cacd6255eb551d47f
lucileu <363146203@qq.com>
2020-01-04 16:42:46 +0800

    组件化

---
+++ 豆瓣评分/cmps/movie-item/movie-item.js
@@ -0,0 +1,30 @@
+// cmps/movie-item/movie-item.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name: {
+      type: String,
+      value: ''
+    },
+    size: {
+      type: Number,
+      value: 0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
